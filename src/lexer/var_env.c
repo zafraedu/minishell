8b771b7fdb5f0e29ec2b66b3bf0ccc46df19145c
Agustin Vincenti <agustin.vincenti92@gmail.com>
2023-11-30 14:54:46 +0100

    fix: problema particular al reemplazar ?

--- src/lexer/var_env.c
+++ src/lexer/var_env.c
@@ -33,20 +33,18 @@ void	process_env_variable(char **data, char **dollar_pos)
 	env_value = NULL;
 	str = NULL;
 	next_dollar_pos = ft_strchr(*dollar_pos + 1, '$');
-	if (*((*dollar_pos) + 1) != '?')  //Esto deberiamos incluirlo al momento de ejecutar el comando
-		process_env_substring(dollar_pos, &str, &sufix, &env_value);
-	else
+	if (*((*dollar_pos) + 1) != '?')  //El rremplazo de $? deberiamos incluirlo al momento de ejecutar el comando
 	{
-			env_value = "$?"; // Deberia ser el exit status
-			if (*((*dollar_pos) + 2) != '\0')
-				sufix = ft_strdup(*dollar_pos + 2);
-			else
-				sufix = "";
+		process_env_substring(dollar_pos, &str, &sufix, &env_value);
+		prefix = ft_substr(*data, 0, *dollar_pos - *data);
+		replace_env_variable(data, prefix, sufix, env_value);
 	}
-	prefix = ft_substr(*data, 0, *dollar_pos - *data);
-	replace_env_variable(data, prefix, sufix, env_value);
 	if (next_dollar_pos)
+	{
 		*dollar_pos = ft_strchr(*data, '$');
+		if (*((*dollar_pos) + 1) == '?')
+			*dollar_pos =  ft_strchr(*dollar_pos + 1, '$');
+	}
 	else
 		*dollar_pos = NULL;
 }
